a4a7e28a7425b1f1f72be74dc15f6bc242fcec74
Denis Chernov <chernoff.cs@gmail.com>
2023-10-31 22:28:28 +0200

    Updating AddNew Form to be usable

--- betterdev/src/Pages/Admin/AddNew/AddNew.js
+++ betterdev/src/Pages/Admin/AddNew/AddNew.js
@@ -1,8 +1,57 @@
 import "./AddNew.css";
 import Exit from "../../../Assets/exit.svg";
 import { useState } from "react";
+import axios from "axios";
 
 const AddNew = (props) => {
+
+  const [developer, setDeveloper] = useState({
+    name: "",
+    services: "",
+    location: "",
+    clients: "",
+    url: "",
+    logo: ""
+  })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(developer);
+    try {
+        const response = await axios.post("your-backend-url", developer);
+    
+        if (response.status === 200) {
+          console.log("Success");
+          console.log(developer);
+          //Clear inputs
+          setDeveloper({
+            name: "",
+            services: "",
+            location: "",
+            clients: "",
+            url: "",
+            logo: ""
+          });
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+      clearForm();
+}
+
+const clearForm = () => {
+  const formInputs = document.querySelectorAll("form input");
+  formInputs.forEach(input => input.value = "");
+  props.showHandler()
+}
+
+const handleDeveloperChange = (e) => {
+    setDeveloper({
+      ...developer,
+      [e.target.name]: e.target.value
+    });
+  }
+
 
   return (
     <div className={`addNewContainer ${props.show}`}>
@@ -10,18 +59,21 @@ const AddNew = (props) => {
         <div className="modalTitle">
           <span>Add new</span>
           <div className="exitBtn">
-            <img src={Exit} alt="Exit" onClick={props.showHandler} />
+            <img src={Exit} alt="Exit" onClick={clearForm} />
           </div>
         </div>
-        <form className="addModalForm">
-          <input type="text" placeholder="Name"/>
-          <input type="text" placeholder="Services"/>
-          <input type="text" placeholder="Location"/>
-          <input type="text" placeholder="Recent clients"/>
-          <input type="text" placeholder="URL"/>
+        <form className="addModalForm" onSubmit={handleSubmit}>
+          <input type="text" placeholder="Name" name="name" onChange={handleDeveloperChange}/>
+          <input type="text" placeholder="Services" name="services" onChange={handleDeveloperChange}/>
+          <input type="text" placeholder="Location" name="location" onChange={handleDeveloperChange}/>
+          <input type="text" placeholder="Recent clients" name="clients" onChange={handleDeveloperChange}/>
+          <input type="text" placeholder="URL" name="url" onChange={handleDeveloperChange}/>
           <div className="modalBTNs">
-            <button className="logo">Logo</button>
-            <button className="add">Add</button>
+            <label className="logo">
+              <input type="file" name="logo" onChange={handleDeveloperChange}/>
+              Logo
+            </label>
+            <input type="submit" className="add" onClick={clearForm} value="Add"></input>
           </div>
         </form>
       </div>
