c37a12f399a9273e00d53cec44284cb48582ccfa
Denis Chernov <chernoff.cs@gmail.com>
2023-11-03 15:57:57 +0200

    Update login form

--- betterdev/src/App.js
+++ betterdev/src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Admin from './Pages/Admin/Admin';
+import AuthGuard from './Pages/Login/AuthGuard';
 
 function App() {
+  const isAuthenticated = sessionStorage.getItem('jwt') ? true : false;
+
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Routes>
-          <Route path='/' element={<BetterDev />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/admin' element={<Admin />} />
+          <Route path="/" element={<BetterDev />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin"
+            element={
+              <AuthGuard isAuthenticated={isAuthenticated}>
+                <Admin />
+              </AuthGuard>
+            }
+          />
         </Routes>
       </div>
     </Router>
--- betterdev/src/Pages/Login/Login.js
+++ betterdev/src/Pages/Login/Login.js
@@ -1,48 +1,72 @@
 import "./Login.css";
-import Logo from "../../Assets/BetterDev_logo.svg"
-import { useState } from "react";
+import Logo from "../../Assets/BetterDev_logo.svg";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  let navigate = useNavigate();
+  const [isLoged, setIsLoged] = useState(false); 
 
-    const [loginData, setLoginData] = useState({
-        username: "",
-        password: ""
-    })
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post("http://localhost:8080/api/v1/auth/authenticate", loginData);
-        
-            if (response.status === 200) {
-              console.log("Success");
-              console.log(loginData);
-            }
-          } catch (error) {
-            console.error("Error:", error);
-          }
+  const redirect = () => {
+    if (isLoged) {
+      navigate("/admin")
     }
+  }
 
-    const handleChange = (e) => {
-        setLoginData({
-          ...loginData,
-          [e.target.name]: e.target.value
-        });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/v1/auth/authenticate",
+        loginData
+      );
+
+      if (response.status === 200) {
+        sessionStorage.setItem("jwt", response.data.token);
+        setIsLoged(true);
+        redirect();
       }
+    } catch (error) {
+      alert("Wrong login and/or password");
+      setIsLoged(false)
+      console.error("Error:", error);
+    }
+  };
 
-
-    return (
-        <div className="login">
-            <img src={Logo} alt="Logo"/>
-            <h1>Login to Admin Panel</h1>
-            <form className="form__container" onSubmit={handleSubmit}>
-                <input type="text" placeholder="Username" onChange={handleChange} name="username" ></input>
-                <input type="password" placeholder="Password" onChange={handleChange} name="password" ></input>
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
-}
+  const handleChange = (e) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <div className="login">
+      <img src={Logo} alt="Logo" />
+      <h1>Login to Admin Panel</h1>
+      <form className="form__container" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={handleChange}
+          name="username"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          name="password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
