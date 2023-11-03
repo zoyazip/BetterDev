177a8dc15609b8baeb9fbb6be1cc88f3fe1b2c72
Denis Chernov <chernoff.cs@gmail.com>
2023-11-03 16:57:48 +0200

    s

--- betterdev/src/Pages/Admin/AdminNav/AdminNav.js
+++ betterdev/src/Pages/Admin/AdminNav/AdminNav.js
@@ -2,10 +2,16 @@ import "./AdminNav.css";
 import Logo from "../../../Assets/BetterDev_logo.svg";
 
 const AdminNav = () => {
+
+    const exit = () => {
+        sessionStorage.removeItem("jwt")
+    }
+
+
     return (
         <div className="adminNav">
             <img src={Logo} alt="Logo"/>
-            <a>Exit</a>
+            <a href="/login" onClick={exit}>Exit</a>
         </div>
     );
 }
